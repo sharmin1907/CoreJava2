0f84dd69302f32b5262936ac709885ac5988ae00
sharmin1907 <sharmin1907@gmail.com>
2019-08-29 00:37:34 -0400

    Pyramid

---
+++ src/surpriseQuiz/Pyramid.java
@@ -0,0 +1,45 @@
+package surpriseQuiz;
+
+public class Pyramid {
+
+    //   Implement a large Pyramid of stars in the screen with java.
+    //
+    //                    *
+    //                   * *
+    //                  * * *
+    //                 * * * *
+    //                * * * * *
+    //               * * * * * *
+
+    //Start Here
+    public static void main(String[] args) {
+        System.out.println("                           *                             ");
+        System.out.println("                          * *                            ");
+        System.out.println("                         * * *                           ");
+        System.out.println("                        * * * *                          ");
+        System.out.println("                       * * * * *                         ");
+        System.out.println("                      * * * * * *                        ");
+        System.out.println("                     * * * * * * *                       ");
+        System.out.println("                    * * * * * * * *                      ");
+        System.out.println("                   * * * * * * * * *                     ");
+        System.out.println("                  * * * * * * * * * *                    ");
+        System.out.println("                 * * * * * * * * * * *                   ");
+        System.out.println("                * * * * * * * * * * * *                  ");
+        System.out.println("               * * * * * * * * * * * * *                 ");
+        System.out.println("              * * * * * * * * * * * * * *                ");
+        System.out.println("             * * * * * * * * * * * * * * *               ");
+        System.out.println("            * * * * * * * * * * * * * * * *              ");
+        System.out.println("           * * * * * * * * * * * * * * * * *             ");
+        System.out.println("          * * * * * * * * * * * * * * * * * *            ");
+        System.out.println("         * * * * * * * * * * * * * * * * * * *           ");
+        System.out.println("        * * * * * * * * * * * * * * * * * * * *          ");
+        System.out.println("       * * * * * * * * * * * * * * * * * * * * *         ");
+        System.out.println("      * * * * * * * * * * * * * * * * * * * * * *        ");
+        System.out.println("     * * * * * * * * * * * * * * * * * * * * * * *       ");
+        System.out.println("    * * * * * * * * * * * * * * * * * * * * * * * *      ");
+        System.out.println("   * * * * * * * * * * * * * * * * * * * * * * * * *     ");
+        System.out.println("  * * * * * * * * * * * * * * * * * * * * * * * * * *    ");
+        System.out.println(" * * * * * * * * * * * * * * * * * * * * * * * * * * *   ");
+
+    }
+}
