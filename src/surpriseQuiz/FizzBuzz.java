ff761ae4c8b2bd199069daf44d8387fc66516920
sharmin1907 <sharmin1907@gmail.com>
2019-08-31 12:57:19 -0400

    FizzBuzz

---
+++ src/surpriseQuiz/FizzBuzz.java
@@ -0,0 +1,17 @@
+package surpriseQuiz;
+
+public class FizzBuzz {
+    //Write a Java program that prints the numbers from 1 to 50. But for multiples of three print "Fizz" instead of the number
+    // and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            if (i % 3 == 0) System.out.println("Fizz");
+            else if (i % 5 == 0) System.out.println("Buzz");
+            else if (i %(3*5)==0) System.out.println("FizzBuzz");
+            else System.out.println(i);
+        }
+    }
+}
+
+
